a5d77a744669b0c6c4a738a0f2280890c90dfd3d
Yu <admin@itc.tokyo>
2021-02-15 00:26:07 +0900

    try test

---
+++ firstOne.js
@@ -0,0 +1,27 @@
+// let, const would be better, because it's not scoped
+
+// let age = 30;
+// age = 31
+// console.log(age)
+
+//const is used mostly and not be changed
+// const age = 30;
+// age = 31
+// console.log(31)
+
+// let sNull = null
+// let sUndef;
+// const sNull2 = null
+
+// console.log(sNull===sUndef)//false
+// console.log(sNull===sNull2)//true
+
+// const name="John"
+// const age= 34
+// const rating=4.5
+// const isNice = true
+// const age0 = 0
+// const sNull = null
+// const y = undefined
+// console.log(`my name is ${name} and age is ${age}`) //` => shift + @
+
---
+++ secondOne.js
@@ -0,0 +1,21 @@
+// const s = "Hello World"
+// console.log(s.length) //1
+// console.log(s.toLowerCase()) //hello world
+// console.log(s.substring(0,5).toUpperCase())//HELLO
+// console.log(s.split('')) //["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d"]
+
+// const newS = "python, javascript, swift"
+// console.log(newS.split(', '))
+
+
+
+// Array
+
+const numbers = new Array(1,2,3,4,5)
+// console.log(numbers)[1,2,3,,,]
+
+numbers[6] = "grapes"
+
+console.log(numbers)
+
+
